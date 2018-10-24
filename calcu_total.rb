b6a2ee7ba34e507f90795cd74a407cbd1602ad41
Siya-ng <ngsiya90@gmail.com>
2018-10-24 17:43:24 +0800

    update to sum

--- calcu_total.rb
+++ calcu_total.rb
@@ -10,7 +10,7 @@
 
 def calculateTotalAmount(amt)
   amt_arr = amt.split(" ").map(&:to_f)
-  total_amount = amt_arr.inject{|a, b | a + b}
+  total_amount = amt_arr.sum
   return '%.2f' % (total_amount * 1.1 * 1.07)
 end
 #  &: is symbol # to proc conversion, part of ruby 1.9 and up
